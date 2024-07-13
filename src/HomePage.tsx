c6722ef90c4c0f87eb19a893fe0aae5c5979c8c3
miracle73 <nwadiaromiraclechukwuma@gmail.com>
2024-07-13 02:46:22 +0200

    Recent changes

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -95,7 +95,7 @@ const HomePage = () => {
           <p className='text-[#E5E9F0] font-[400] text-[18px] max-xl:text-[16px] max-lg:text-[14px] max-md:text-[12px] font-[Poppins]'>Hi all. I am</p>
           <p className='text-[#E5E9F0] font-[600] text-[52px] max-xl:text-[48px] max-lg:text-[44px] max-md:text-[40px] max-sm:text-[28px] max-xsm:text-[20px] font-[Poppins]'>Olawale O. Omoyeni,</p>
 
-          <p className='text-[#FFFFFF] font-[400] text-[16px] max-xl:text-[14px] max-lg:text-[12px] max-md:text-[10px] font-[Poppins]'>AI expert currently pursuing a PhD in Information Technology & Artificial Intelligence, with hands-on experience in machine learning, data analysis, and statistical modeling.</p>
+          <p className='text-[#FFFFFF] font-[400] text-[16px] max-xl:text-[14px] max-lg:text-[12px] max-md:text-[10px] font-[Poppins]'>An AI expert currently pursuing a PhD in Information Technology & Artificial Intelligence, with hands-on experience in machine learning, data analysis, and statistical modeling.</p>
           <div className='h-[55px] w-[190px] max-sm:w-fit max-sm:h-fit max-sm:py-4 max-sm:px-4 border border-[#585858] rounded-[80px] flex flex-row justify-center items-center'>
             <p className='text-[#FFFFFF] font-[500] text-[15px] max-xl:text-[13px] max-lg:text-[11px] max-md:text-[9px] font-[Poppins] text-center'>Olawale O. Omoyeni</p>
           </div>
